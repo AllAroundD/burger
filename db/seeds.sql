0c5bd3cd7a5f93e25cc21160d56bad52213dee4d
AllAroundD <AllAroundDWeb@gmail.com>
2020-06-25 22:44:41 -0400

    initial push

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Double Cheeseburger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Cheeseburger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Big Mac", false);
